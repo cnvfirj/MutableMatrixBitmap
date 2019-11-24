cd68267b6dfadec508d89c35e9d0e635864ecb9f
Ihor <cnvfirj@gmail.com>
2019-11-24 19:50:28 +0300

    in home

---
+++ mutmatrix/src/main/java/com/example/mutmatrix/DeformMat.java
@@ -0,0 +1,104 @@
+package com.example.mutmatrix;
+
+import android.content.Context;
+import android.graphics.Matrix;
+import android.graphics.PointF;
+import android.view.MotionEvent;
+
+import com.example.mutmatrix.actions.Base;
+import com.example.mutmatrix.actions.Deform;
+import com.example.mutmatrix.actions.Rotate;
+import com.example.mutmatrix.actions.Scale;
+import com.example.mutmatrix.actions.Translate;
+
+import static com.example.mutmatrix.Massages.ERROR;
+
+public class DeformMat {
+
+    public enum Command{
+        TRANSLATE,
+        SCALE,
+        ROTATE,
+        DEFORM,
+        DEFORM_CONT,
+        MIRROR,
+        RESET,
+        NULLABLE
+    }
+
+    private Base base, translate, scale, rotate, deform;
+
+    private CompRep repository;
+
+    private Command command;
+
+
+    public DeformMat(Context context) {
+        repository = new CompRep();
+        translate = new Translate(context).rep(repository);
+        rotate = new Rotate(context).rep(repository);
+        scale = new Scale(context).rep(repository);
+        deform = new Deform(context).rep(repository);
+    }
+
+    public DeformMat reset(){
+        repository = new CompRep();
+        translate.rep(repository);
+        rotate.rep(repository);
+        scale.rep(repository);
+        deform.rep(repository);
+        return this;
+    }
+
+    public Matrix matrix(Matrix matrix){
+        if(matrix==null)matrix = new Matrix();
+        matrix.reset();
+        matrix.setPolyToPoly(repository.getSrc(),0,repository.getDst(),0,4);
+        matrix.postScale(repository.getScale(),repository.getScale());
+        matrix.postRotate(repository.getRotate());
+        matrix.postTranslate(repository.getTranslate().x,repository.getTranslate().y);
+        return matrix;
+    }
+
+
+    public DeformMat command(Command command){
+        switch (command){
+            case TRANSLATE:
+                base = translate;
+                break;
+            case ROTATE:
+                base = rotate;
+                break;
+            case SCALE:
+                base = scale;
+                break;
+            case DEFORM:
+                base = deform;
+        }
+        return this;
+    }
+
+    public DeformMat event(MotionEvent event){
+        if(base!=null){
+            base.touch(event);
+        }else ERROR("base mutable null");
+        return this;
+    }
+
+    public DeformMat bitmap(PointF b){
+        repository.setBitmap(b);
+        return this;
+    }
+
+    public void setRepository(CompRep rep) {
+        this.repository = rep;
+        translate.rep(rep);
+        rotate.rep(rep);
+        scale.rep(rep);
+        deform.rep(rep);
+    }
+
+    public CompRep getRepository(){
+        return repository;
+    }
+}
